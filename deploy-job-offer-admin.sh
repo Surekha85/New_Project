9d29eb1bc697c87a9ae16e81f400fee6c6fe2701
Surekha85 <surekhasandhu96@gmail.com>
2026-02-25 17:23:11 +0530

    create assistants.

--- deploy-job-offer-admin.sh
+++ deploy-job-offer-admin.sh
@@ -66,7 +66,7 @@ aws cloudformation deploy \
     CandidateCrossAccountRoleArn=arn:aws:iam::779803046578:role/jobsyme-candidates-$STAGE-CrossAccountDynamoRole \
     AssistantCrossAccountRoleArn=arn:aws:iam::779803046578:role/jobsyme-assistants-$STAGE-CrossAccountAccessRole \
     CandidatesTableName=jobsyme-$STAGE-candidates \
-    AssistantsTableName=jobsyme-$STAGE-assistants \
+    AssistantsTableName=jobsyme-delta-assistants \
     JobApplicationsTableName=jobsyme-$STAGE-job-applications \
     PortfolioTableName=jobsyme-$STAGE-portfolio \
     GitHubActivitiesTableName=jobsyme-$STAGE-github-activities \
